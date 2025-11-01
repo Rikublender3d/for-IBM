b027b18867fd818998a1978fce88050703f964e7
“Rikublender3d” <mikublender3d@gmail.com>
2025-11-01 06:06:43 -0400

    feat:add-lab-3

---
+++ followersCount/followers_count.js
@@ -0,0 +1,16 @@
+let count = 0
+function increaseCount() {
+    count++;
+    displayCount();
+    checkCountValue();
+}
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count;
+}
+function checkCountValue() {
+    if (count === 10) {
+        alert("あなたのInstagram投稿が10人のフォロワーを獲得しました！おめでとうございます！");  
+    } else if (count === 20) {
+        alert("あなたのInstagram投稿が20人のフォロワーを獲得しました！その調子です！");
+    }
+}
